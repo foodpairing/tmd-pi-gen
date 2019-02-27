3240171a722801b03e3b62ef6b88211b595c9a9a
Davy Hollevoet <davy@foodpairing.com>
2019-02-27 12:26:52 +0100

    work towards an image that works in read-only

---
+++ bartendro/02-readonly-root/02-run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+# install tmd_bartendro python package
+on_chroot <<EOF
+# generate SSH host keys (yes, these will be the same for all devices that use the same image build)
+dpkg-reconfigure openssh-server
+EOF
+
+# from http://hallard.me/raspberry-pi-read-only/
+rm -rf "${ROOTFS_DIR}/var/lib/dhcp/" "${ROOTFS_DIR}/var/lib/dhcpcd5" "${ROOTFS_DIR}/var/run" "${ROOTFS_DIR}/var/spool" "${ROOTFS_DIR}/var/lock" "${ROOTFS_DIR}/etc/resolv.conf"
+ln -s /tmp "${ROOTFS_DIR}/var/lib/dhcp"
+ln -s /tmp "${ROOTFS_DIR}/var/lib/dhcpcd5"
+ln -s /tmp "${ROOTFS_DIR}/var/run"
+ln -s /tmp "${ROOTFS_DIR}/var/spool"
+ln -s /tmp "${ROOTFS_DIR}/var/lock"
+ln -s /tmp/dhcpcd.resolv.conf "${ROOTFS_DIR}/etc/resolv.conf"
+
+rm -rf "${ROOTFS_DIR}/var/lib/nginx"
+ln -s /tmp "${ROOTFS_DIR}/var/lib/nginx"
+
+install -v -m 644 files/tmpfiles.d.tmd.conf "${ROOTFS_DIR}/etc/tmpfiles.d/tmd.conf"
