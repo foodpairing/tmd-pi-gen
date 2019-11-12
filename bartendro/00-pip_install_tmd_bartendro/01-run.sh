4abd7a039bc6c6e7decdc4a831ae6a6aaa2b10c3
Davy Hollevoet <davy@foodpairing.com>
2019-11-12 15:47:05 +0100

    ditch uwsgi in favor of a bare systemd service
    uwsgi + python-socketio was not playing ball. The socketio connection could
    not be created once in a thread

--- bartendro/00-pip_install_tmd_bartendro/01-run.sh
+++ bartendro/00-pip_install_tmd_bartendro/01-run.sh
@@ -11,12 +11,13 @@ EOF
 # create config dir and copy example config
 install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/${CONFIG_DIR}"
 install -v -o 1000 -g 1000 -m 600 ${ROOTFS_DIR}/${BARTENDRO_DIR}/example/* ${ROOTFS_DIR}/${CONFIG_DIR}/
-touch ${ROOTFS_DIR}/${CONFIG_DIR}/touch-reload
-chown 1000:1000 ${ROOTFS_DIR}/${CONFIG_DIR}/touch-reload
 
-# install uwsgi config
-install -v -m 644 files/bartendro.uwsgi "${ROOTFS_DIR}/etc/uwsgi/apps-available/bartendro.ini"
-ln -sf /etc/uwsgi/apps-available/bartendro.ini "${ROOTFS_DIR}/etc/uwsgi/apps-enabled/bartendro.ini"
+# install systemd service
+install -v -m 644 files/bartendro.service "${ROOTFS_DIR}/etc/systemd/system/bartendro.service"
+
+on_chroot <<EOF
+systemctl enable bartendro
+EOF
 
 # install nginx config
 install -v -m 644 files/bartendro.nginx "${ROOTFS_DIR}/etc/nginx/sites-available/default"
